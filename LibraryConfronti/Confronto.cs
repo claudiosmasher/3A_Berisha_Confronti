02bdffecc90bda0980d377003183343800209485
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-23 08:55:40 +0100

    Primo test zona rossa risultato positivo.

--- LibraryConfronti/Confronto.cs
+++ LibraryConfronti/Confronto.cs
@@ -4,6 +4,9 @@ namespace LibraryConfronti
 {
     public class Confronto
     {
-
+        public static int Max(int n1, int n2)
+        {
+            return 0;
+        }
     }
 }
---
+++ UnitTestProject1/Confronto_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryConfronti;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Confronto_Tests
+    {
+        [TestMethod]
+        public void TestMax()
+        {
+            int n1 = 0;
+            int n2 = 7;
+            int max_aspettato = 7;
+
+            int max_calcolato = LibraryConfronti.Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspettato, max_calcolato);
+        }
+    }
+}
